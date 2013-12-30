37f107d354e64fc4a88cce743948e14e90ceb305
Tim Shultis <tim@morsekode.com>
2013-12-30 13:35:31 -0600

    Initial Commit

---
+++ Delegates/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Delegates
+//
+//  Created by timshultis on 12/30/13.
+//  Copyright (c) 2013 timshultis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Delegates/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Delegates
+//
+//  Created by timshultis on 12/30/13.
+//  Copyright (c) 2013 timshultis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
